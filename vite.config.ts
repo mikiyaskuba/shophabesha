ff4b82eebb8eba8f057c3ef966c6762348fbf00f
mikiyaskuba <bigdream070707@gmail.com>
2025-12-05 15:18:31 +0300

    adding inventory and other features

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: 5000,
+    host: true,
+    port: 5173,
     strictPort: true,
+    allowedHosts: ["*"], // allow all hosts (required for Replit)
     hmr: {
       protocol: "wss",
       clientPort: 443,
+      host: "0.0.0.0",
     },
   },
 });
